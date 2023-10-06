16f3fac91caaf874274bf40938fd5d5aff543674
florencionath <nathalia.pinto@sptech.school>
2023-10-05 22:36:55 -0300

    tela login V1

---
+++ Site/Tela login/login.js
@@ -0,0 +1,17 @@
+function acessar(){
+
+    var email = ipt_email.value;
+    var senha = ipt_senha.value;
+    
+    if (email.indexOf('@.') >= 0 &&
+         (email.indexOf('.com') >= 0 ||
+         email.indexOf('.br') >= 0)) {div_mensagem.innerHTML = 'Email autorizado <br>'}
+         else {div_mensagem.innerHTML = 'Coloque um email valido <br>'}
+    
+    
+    if (senha.length == 8) {
+          div_mensagem.innerHTML += `Sua senha está correta <br>`;
+        } else {
+          div_mensagem.innerHTML += `Sua senha deve possuir 8 digitos! <br>`;
+        }
+}
