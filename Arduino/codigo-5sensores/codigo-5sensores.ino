437c166166a3424a6e8268b03fa99d60b4ea6101
florencionath <nathalia.pinto@sptech.school>
2023-10-25 00:38:24 -0300

    código arduino comentado deixando apenas o ht11

--- Arduino/codigo-5sensores/codigo-5sensores.ino
+++ Arduino/codigo-5sensores/codigo-5sensores.ino
@@ -4,15 +4,15 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include "DHT.h"
-#define DHTPIN A0
-#define LM35PIN A5 
-#define LUMIPIN A1
- #define CHAVPIN 7
+ #define DHTPIN A0 // AZUL
+// #define LM35PIN A5 AMARELO NEON
+// #define LUMIPIN A1 VINHO
+//  #define CHAVPIN 7 PRETO, LARANJA GND E AMARELO MOSTARDA 5V
 DHT dht(DHTPIN, DHT11);
 void setup()
 {
 pinMode(DHTPIN, INPUT);
-pinMode(CHAVPIN, INPUT);
+// pinMode(CHAVPIN, INPUT);
 Serial.begin(9600);
 dht.begin();
 }
@@ -24,23 +24,23 @@ Serial.print(dht11_umidade);
 Serial.print(";");
 Serial.print(dht11_temperatura);
 Serial.print(";");
- float luminosidade = analogRead(LUMIPIN);
- Serial.print(luminosidade);
- Serial.print(";");
- float lm35_temperatura = analogRead(LM35PIN);
- lm35_temperatura = lm35_temperatura * 0.00488;
- lm35_temperatura = lm35_temperatura * 100;
- Serial.print(lm35_temperatura);
- Serial.print(";");
- int chave = digitalRead(7);
- if (chave == 0)
- {
- Serial.print("1");
- }
- else
- {
- Serial.print("0");
- }
+//  float luminosidade = analogRead(LUMIPIN);
+//  Serial.print(luminosidade);
+//  Serial.print(";");
+//  float lm35_temperatura = analogRead(LM35PIN);
+//  lm35_temperatura = lm35_temperatura * 0.00488;
+//  lm35_temperatura = lm35_temperatura * 100;
+//  Serial.print(lm35_temperatura);
+//  Serial.print(";");
+//  int chave = digitalRead(7);
+//  if (chave == 0)
+//  {
+//  Serial.print("1");
+//  }
+//  else
+//  {
+//  Serial.print("0");
+ //}
 Serial.println();
 delay(20000);
 }
--- Site/Site-Institucional/js/login.js
+++ Site/Site-Institucional/js/login.js
@@ -3,24 +3,59 @@ function acessar() {
   var email = ipt_email.value;
   var senha = ipt_senha.value;
 
-  if (email.indexOf('@') >= 0 && (email.indexOf('.com') >= 0 ||  email.indexOf('.br') >= 0)) {
-    
-    
-  if (senha.length >= 8) {
-    
-    window.location.href = `./dashbord.html`;//Com ele o link que você fornecer abre em outra aba
-  }
-   
+  if (email.indexOf('@') >= 0 && (email.indexOf('.com') >= 0 || email.indexOf('.br') >= 0)) {
+
+
+    if (senha.length >= 8) {
+
+      window.location.href = `./dashbord.html`;//Com ele o link que você fornecer abre em outra aba
+    }
+
   }
   else {
     div_mensagem.innerHTML = '<p class="erro">E-mail ou Senha inválidos<br></p>'
   }
 
 
-//   if (senha.length >= 8) {
-//     div_mensagem.innerHTML += `<p class="sucesso">Sua senha está correta <br> </p>`;
-//     window.location.href = `./dashbord.html`;
-//   } else {
-//     div_mensagem.innerHTML += `<p class="erro">Sua senha deve possuir ao menos 8 caracteres <br> </p>`;
-//   }
+  //   if (senha.length >= 8) {
+  //     div_mensagem.innerHTML += `<p class="sucesso">Sua senha está correta <br> </p>`;
+  //     window.location.href = `./dashbord.html`;
+  //   } else {
+  //     div_mensagem.innerHTML += `<p class="erro">Sua senha deve possuir ao menos 8 caracteres <br> </p>`;
+  //   }
+}
+
+function novaSenha() {
+  var email = ipt_email.value;
+  var container = container_login;
+
+  if (email.indexOf('@') >= 0 && (email.indexOf('.com') >= 0 || email.indexOf('.br') >= 0)) {
+    container.innerHTML = `
+    <h1 class="titulo_recuperar_senha">Recuperar senha</h1>
+    
+            <div class="container_input">
+            
+            <div class="container_email">
+                <p class="p_nova_senha"> Digite o código enviado ao e-mail <span id="ipt_email_recupera"> </span> </p>
+                    <input class="ipt_codigo" id="ipt_codigo" type="number" placeholder="0000"><br>
+                    <p class="p_nova_senha"> Digite a nova senha</p>
+                    <input class="ipt_nova_senha" type="password" placeholder="xxxxxxxx">
+                </div>
+
+                <button class="botao" onclick="recuperar()">Recuperar</button><br>
+                <div id="div_mensagem"></div>
+            </div>
+    `
+    ipt_email_recupera.innerHTML = email;
+  } else {
+    div_mensagem.innerHTML = `Insira o e-mail cadastrado`
+    div_mensagem.style = "color: red"
+  }
+}
+function recuperar() {
+  const codigo = ipt_codigo;
+  if(codigo.value === 1818){
+    console.log('foi')
+  }
+
 }
